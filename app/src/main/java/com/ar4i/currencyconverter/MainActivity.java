d6427c4983b221935f1f54997f7f1fd4810b150e
Pabls <pabls@yandex.ru>
2019-05-22 22:25:11 +0300

    добавил simpleXml

--- app/src/main/java/com/ar4i/currencyconverter/MainActivity.java
+++ app/src/main/java/com/ar4i/currencyconverter/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    
+    int order = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
